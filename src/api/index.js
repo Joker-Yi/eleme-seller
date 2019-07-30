48be826dd37a89dc80f94ca3ba96ad9e32c58fa2
Joker-Yi <735734221@qq.com>
2019-07-30 19:42:32 +0800

    创建header组件

---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Yee on 2019/7/25.
+ */
+import {get} from './helper'
+
+const getSeller = get('/api/seller');
+const getGoods = get('/api/goods');
+const getRatings = get('/api/ratings');
+
+export {
+  getSeller,
+  getGoods,
+  getRatings
+}
+
+
+
+
+
+
+
+
+
+
